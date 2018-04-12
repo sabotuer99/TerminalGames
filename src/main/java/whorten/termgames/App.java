e87293013604421b68f312492e50be4fffd8d39d
troy <sabotuer99@gmail.com>
2018-04-11 22:29:44 -0700

    refactored main to demo KeyboardEventDriver

--- src/main/java/whorten/termgames/App.java
+++ src/main/java/whorten/termgames/App.java
@@ -2,6 +2,9 @@ package whorten.termgames;
 
 import java.io.IOException;
 
+import whorten.termgames.events.keyboard.KeyEvent;
+import whorten.termgames.events.keyboard.KeyEventType;
+import whorten.termgames.events.keyboard.KeyboardEventDriver;
 import whorten.termgames.utils.Keys;
 import whorten.termgames.utils.TerminalNavigator;
 
@@ -9,96 +12,69 @@ import whorten.termgames.utils.TerminalNavigator;
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
 	private static TerminalNavigator nav = new TerminalNavigator(System.out);
-	
-    public static void main( String[] args ) throws IOException
-    {
-    	reset();
-        int c = 0xFF;
-        while(c != 'Q' && c != 'q'){
-        	
-        	if ( System.in.available() != 0 ) {
-                c = System.in.read();
-                
-            	switch(c){
-            	case 0x1B:
-            		c = System.in.read();
-            		if(c == '['){
-            			c = System.in.read();
-            		}
-            		switch(c){
-            		case 'B':
-	            	case Keys.DOWN_ARROW_BYTE:
-	            		nav.cursorDown();
-	            		System.out.print(' ');
-	            		nav.cursorBack();
-	            		break;
-	            	case 'A':
-	            	case Keys.UP_ARROW_BYTE:
-	            		nav.cursorUp();
-	            		System.out.print(' ');
-	            		nav.cursorBack();
-	            		break;
-	            	case 'C':
-	            	case Keys.RIGHT_ARROW_BYTE:
-	            		nav.cursorForward();
-	            		System.out.print(' ');
-	            		nav.cursorBack();
-	            		break;
-	            	case 'D':
-	            	case Keys.LEFT_ARROW_BYTE:
-	            		nav.cursorBack();
-	            		System.out.print(' ');
-	            		nav.cursorBack();
-	            		break;
-            		}
-            		break;
-            	case 'k':
-            		nav.cursorDown();
-            		System.out.print(' ');
-            		nav.cursorBack();
-            		break;
-            	case 'i':
-            		nav.cursorUp();
-            		System.out.print(' ');
-            		nav.cursorBack();
-            		break;
-            	case 'l':
-            		nav.cursorForward();
-            		System.out.print(' ');
-            		nav.cursorBack();
-            		break;
-            	case 'j':
-            		nav.cursorBack();
-            		System.out.print(' ');
-            		nav.cursorBack();
-            		break;
-            	case 's':
-            		reset();
-            		break;
-            	default:
-            		System.out.print((char)c);
-            		System.out.print('X');
-            		break;
-            	}
-            }
-        	
-        }
-        System.out.println("PEACE!");
-    }
 
-	private static void reset(){
-		StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws IOException {
+		reset();
+		KeyboardEventDriver ked = new KeyboardEventDriver.Builder()
+				.withInputStream(System.in)
+				.withListener((KeyEvent ke) -> handleKeyEvent(ke))
+				.withStopCharacter('Q')
+				.withStopCharacter('q')
+				.build();
+
+		ked.listen();
+		System.out.println("PEACE!");
+	}
+
+	private static void handleKeyEvent(KeyEvent ke) {
+		// ignore up events for now
+		if(ke.getKeyEventType() == KeyEventType.UP) return;
 		
-		for(int row = 0; row < 23; row++){
-			for(int col = 0; col < 79; col++){
+		switch (ke.getKey()) {
+		case "K":
+		case Keys.DOWN_ARROW:
+			nav.cursorDown();
+			System.out.print(' ');
+			nav.cursorBack();
+			break;
+		case "I":
+		case Keys.UP_ARROW:
+			nav.cursorUp();
+			System.out.print(' ');
+			nav.cursorBack();
+			break;
+		case "L":
+		case Keys.RIGHT_ARROW:
+			nav.cursorForward();
+			System.out.print(' ');
+			nav.cursorBack();
+			break;
+		case "J":
+		case Keys.LEFT_ARROW:
+			nav.cursorBack();
+			System.out.print(' ');
+			nav.cursorBack();
+			break;
+		default:
+			System.out.print(ke.getKey());
+			nav.cursorBack();
+			break;
+		}
+	}
+	
+	private static void reset() {
+		StringBuilder sb = new StringBuilder();
+
+		for (int row = 0; row < 23; row++) {
+			for (int col = 0; col < 79; col++) {
 				sb.append('#');
 			}
 			sb.append("\n\r");
 		}
-		
+
 		nav.positionCursor(1, 1);
 		System.out.print(sb);
 		nav.positionCursor(1, 1);
