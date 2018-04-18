a22d9dc974063b2c765c5da387f617aeba8cae1b
troy <sabotuer99@gmail.com>
2018-04-18 01:15:03 -0700

    Screw it, less is more

--- src/main/java/whorten/termgames/utils/SoundPlayer.java
+++ src/main/java/whorten/termgames/utils/SoundPlayer.java
@@ -3,13 +3,7 @@ package whorten.termgames.utils;
 import java.io.IOException;
 import java.io.InputStream;
 
-import javax.sound.midi.MidiChannel;
-import javax.sound.midi.MidiDevice;
-import javax.sound.midi.MidiSystem;
-import javax.sound.midi.Receiver;
 import javax.sound.midi.Sequencer;
-import javax.sound.midi.Synthesizer;
-import javax.sound.midi.Transmitter;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
@@ -21,28 +15,7 @@ import javax.sound.sampled.UnsupportedAudioFileException;
 public class SoundPlayer {
 
 	private Sequencer sequencer = null;
-	private Synthesizer synthesizer = null;
-	private SoundPlayer(){
-		try{
-		     MidiDevice.Info[] infos = MidiSystem.getMidiDeviceInfo();
-		     for (int i = 0; i < infos.length; i++){
-		    	 Object mysteryDevice = MidiSystem.getMidiDevice(infos[i]);
-		    	 if(mysteryDevice instanceof Sequencer) {
-		    			 sequencer = (Sequencer) mysteryDevice;
-		    	 }
-		    	 if(mysteryDevice instanceof Synthesizer) {
-		    			 synthesizer = (Synthesizer) mysteryDevice;
-		    	 }
-		     }
-		     sequencer.open();
-		     synthesizer.open();
-		     Receiver synthReceiver = synthesizer.getReceiver();
-		     Transmitter seqTransmitter = sequencer.getTransmitter();
-		     seqTransmitter.setReceiver(synthReceiver);
-		} catch (Exception ex){
-			//gulp
-		}
-	}
+	private SoundPlayer(){}
 	
 	public void stopMidi(){
 		if(sequencer != null && sequencer.isOpen() && sequencer.isRunning()){
@@ -51,41 +24,20 @@ public class SoundPlayer {
 	}
 	
 	public void playMidi(InputStream midiFile, boolean loop){
-
-//		try{
-//	     sequencer.stop();     	     
-//	     sequencer.setSequence(MidiSystem.getSequence(midiFile));
-//	     sequencer.start();
-//	     MidiChannel[] channels = synthesizer.getChannels();
-//	     for (int i = 0; i < channels.length; i++)
-//	          channels[i].controlChange(7, 30);
-//		
-//		} catch (Exception ex) {}
 		
-		//		if(sequencer == null){
-//			throw new IllegalStateException("cannot play midi, no sequencer set.");
-//		}		
-//		
-//		int loopCount = loop ? Sequencer.LOOP_CONTINUOUSLY : 0;
-//		try{		
-//			sequencer.open();
-//			sequencer.setSequence(midiFile);
-//			sequencer.setLoopCount(loopCount);
-//			
-//			for(Track track : sequencer.getSequence().getTracks()){	
-//				track.add(new MidiEvent(
-//					    new ShortMessage(ShortMessage.CONTROL_CHANGE, 7, 30),
-//					    0));
-//				track.add(new MidiEvent(
-//					    new ShortMessage(ShortMessage.CONTROL_CHANGE, 39, 0),
-//					    0));
-//			}
-//			
-//			sequencer.start();
-//			
-//		} catch (Exception ex){
-//			// gulp
-//		}
+		if(sequencer == null){
+			throw new IllegalStateException("cannot play midi, no sequencer set.");
+		}		
+		
+		int loopCount = loop ? Sequencer.LOOP_CONTINUOUSLY : 0;
+		try{		
+			sequencer.open();
+			sequencer.setSequence(midiFile);
+			sequencer.setLoopCount(loopCount);
+			sequencer.start();			
+		} catch (Exception ex){
+			// gulp
+		}
 	}
 	
 	
