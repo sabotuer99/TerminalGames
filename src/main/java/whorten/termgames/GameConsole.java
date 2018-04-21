38b7f543b526b534562fb4db1982b0f11d336699
troy <sabotuer99@gmail.com>
2018-04-20 20:54:34 -0700

    starting to make menu functional

--- src/main/java/whorten/termgames/GameConsole.java
+++ src/main/java/whorten/termgames/GameConsole.java
@@ -9,7 +9,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
-import java.util.TreeSet;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -36,6 +35,7 @@ import whorten.termgames.sounds.events.MidiStartEvent;
 import whorten.termgames.sounds.events.MidiStopEvent;
 import whorten.termgames.sounds.events.PlaySoundEvent;
 import whorten.termgames.utils.CircularList;
+import whorten.termgames.utils.Keys;
 import whorten.termgames.utils.StringUtils;
 
 /**
@@ -46,16 +46,21 @@ public class GameConsole {
 	private static GameConsole instance = new GameConsole();
 	private KeyboardEventDriver ked;
 	private static ExecutorService pool = Executors.newCachedThreadPool();
+	private volatile boolean stillPlaying = true;
 	private Renderer renderer = new OutputStreamRenderer(System.out, 80, 24);
 	private final EventBus eventBus = new EventBus();
 	private final SoundPlayer soundPlayer = getSoundPlayer();
 	private ClassLoader classLoader = ClassLoader.getSystemClassLoader();
 	private Map<String, byte[]> cachedBytes = new HashMap<>();
-	private Map<String, Game> games = new TreeMap<>(); 
-	
+	private List<String> gameNames = new CircularList<>();
+	private Map<String, Game> games = new TreeMap<>();
+	private volatile boolean gameIsRunning;
+	private Game currentGame = null;
+	private int gameIndex = 0;
+
 	public static void main(String[] args) throws IOException, InterruptedException {
 		
-		initEventSystem();
+		initEventSystem(instance);
 		
 		instance.runInThreadPool(() -> {
 			try { instance.ked.listen();} 	
@@ -64,9 +69,21 @@ public class GameConsole {
 
 		instance.loadGames();			
 		instance.renderMenu();
-		instance.renderGameSelector(0);
+		instance.renderGameSelector(instance.gameIndex);
 		
-		Thread.sleep(5000);
+		if(instance.games.size() == 0){
+			instance.stillPlaying = false;
+			Thread.sleep(5000);
+		} else {
+			instance.currentGame = instance.games.get(
+					instance.gameNames.get(instance.gameIndex));
+		}
+			
+		
+		
+		while(instance.stillPlaying){
+			Thread.sleep(100);
+		}
 		
 //     
 //		for(int i = 0; i < 5; i++){
@@ -75,66 +92,76 @@ public class GameConsole {
 //			instance.soundPlayer.close();
 //		}
 		
-		Glyph bodyGlyph =  new Glyph.Builder("◆")
-	            .withForegroundColor(FgColor.LIGHT_YELLOW)
-	            .withBackgroundColor(BgColor.GREEN)
-	            .build();
-		
-		for(int row = 1; row <= 24; row++){
-			for(int col = 1; col <= 80; col++){
-				instance.renderer.drawAt(row,col,bodyGlyph);
-			}
-		}
-		
+		fancyClose();		
 		instance.ked.die();
 		pool.shutdown();
+	}
+
+	private static void fancyClose() {
+		Glyph bodyGlyph = new Glyph.Builder("◆").withForegroundColor(FgColor.LIGHT_YELLOW)
+				.withBackgroundColor(BgColor.GREEN).build();
+
+		for (int row = 1; row <= 24; row++) {
+			for (int col = 1; col <= 80; col++) {
+				instance.renderer.drawAt(row, col, bodyGlyph);
+			}
+		}
 	}
 
 	private void loadGames() {
 		ClassFinder classFinder = new ClassFinder(Game.class);
 		Set<String> gameClassNames = classFinder.getClasses();
 		games.remove("whorten.termgames.games.Game");
-		
-		for(String gameClassName : gameClassNames){
+
+		for (String gameClassName : gameClassNames) {
 			try {
 				Game game = (Game) Class.forName(gameClassName).newInstance();
 				games.put(game.getDisplayName(), game);
 			} catch (Exception e) {
-				//gulp
+				// gulp
 			}
 		}
 	}
 
 	private void renderGameSelector(int index) {
 		int offset = (renderer.getCanvasWidth() - 30) / 2;
-		List<String> displayNames = new CircularList<>();
-		for(String name : games.keySet()){
-			displayNames.add(name);
+		if(games.size() == 0){
+			GlyphString error = new GlyphString.Builder(pad("ERROR: no games loaded!", 30))
+					.withFgColor(255,0,0).isBold(true).build();
+			renderer.drawAt(13, offset, error);
+			return;
 		}
-		
+				
+		gameNames = new CircularList<>();
+		for (String name : games.keySet()) {
+			gameNames.add(name);
+		}
+
 		String[] names = new String[7];
-		for(int i = 0; i < 7; i++){
-			names[i] = displayNames.get(i - 3);
+		for (int i = 0; i < 7; i++) {
+			names[i] = gameNames.get(i - 3);
 		}
-		
+
 		GlyphString.Builder menuBuilder = new GlyphString.Builder(" ");
 		GlyphString[] nameGS = new GlyphString[7];
-		nameGS[0] = menuBuilder.withBaseString(pad(names[0],30)).withFgColor(64, 64, 64).build();
-  		nameGS[1] = menuBuilder.withBaseString(pad(names[1],30)).withFgColor(128,128,128).build();
-  		nameGS[2] = menuBuilder.withBaseString(pad(names[2],30)).withFgColor(192,192,192).build();
-  		nameGS[3] = menuBuilder.withBaseString(pad(names[3],30)).withFgColor(FgColor.WHITE).isBold(true).withBgColor(0,0,64).build();
-  		nameGS[4] = menuBuilder.withBaseString(pad(names[4],30)).withFgColor(192,192,192).isBold(false).clearBgColor().build();		                                                            
-  		nameGS[5] = menuBuilder.withBaseString(pad(names[5],30)).withFgColor(128,128,128).build();
-  		nameGS[6] = menuBuilder.withBaseString(pad(names[6],30)).withFgColor(64, 64, 64).build();
-  		for(int i = 0; i < 7; i ++){
-  			renderer.drawAt(13 + i, offset, nameGS[i]);
-  		}
+		nameGS[0] = menuBuilder.withBaseString(pad(names[0], 30)).withFgColor(64, 64, 64).build();
+		nameGS[1] = menuBuilder.withBaseString(pad(names[1], 30)).withFgColor(128, 128, 128).build();
+		nameGS[2] = menuBuilder.withBaseString(pad(names[2], 30)).withFgColor(192, 192, 192).build();
+		nameGS[3] = menuBuilder.withBaseString(pad(names[3], 30)).withFgColor(FgColor.WHITE).isBold(true)
+				.withBgColor(0, 0, 64).build();
+		nameGS[4] = menuBuilder.withBaseString(pad(names[4], 30)).withFgColor(192, 192, 192).isBold(false)
+				.clearBgColor().build();
+		nameGS[5] = menuBuilder.withBaseString(pad(names[5], 30)).withFgColor(128, 128, 128).build();
+		nameGS[6] = menuBuilder.withBaseString(pad(names[6], 30)).withFgColor(64, 64, 64).build();
+		for (int i = 0; i < 7; i++) {
+			renderer.drawAt(13 + i, offset, nameGS[i]);
+		}
 	}
 
 	private String pad(String string, int width) {
-		if(string.length() > 30){
+		if (string.length() > 30) {
 			return string.substring(0, 27) + "...";
-		}		
+		}
 		int left = (30 - string.length()) / 2;
 		int right = 30 - string.length() - left;
 		return StringUtils.repeat(" ", left) + string + StringUtils.repeat(" ", right);
@@ -146,101 +173,131 @@ public class GameConsole {
 		renderer.clearScreen();
 		GameBorder gb = defaultGameBorder(renderer);
 		renderer.drawGlyphCollection(gb.getGlyphCoords());
-		
+
 		GlyphString.Builder menuBuilder = new GlyphString.Builder(" ");
-  		String title1 = " _____                      ____                           ";
-  		String title2 = "|_   _|__ _ __ _ __ ___    / ___| __ _ _ __ ___   ___  ___ ";
-  		String title3 = "  | |/ _ \\ '__| '_ ` _ \\  | |  _ / _` | '_ ` _ \\ / _ \\/ __|";
-  		String title4 = "  | |  __/ |  | | | | | | | |_| | (_| | | | | | |  __/\\__ \\";
-  		String title5 = "  |_|\\___|_|  |_| |_| |_|  \\____|\\__,_|_| |_| |_|\\___||___/";
-  		GlyphString gsTitle1 = menuBuilder.withBaseString(title1).withFgColor(0,0,255).build();
-  		GlyphString gsTitle2 = menuBuilder.withBaseString(title2).withFgColor(0,64,255).build();
-  		GlyphString gsTitle3 = menuBuilder.withBaseString(title3).withFgColor(0,128,255).build();
-  		GlyphString gsTitle4 = menuBuilder.withBaseString(title4).withFgColor(0,192,255).build();
-  		GlyphString gsTitle5 = menuBuilder.withBaseString(title5).withFgColor(0,255,255).build();		                                                            
-  		renderer.drawAt(2, 11, gsTitle1);
-  		renderer.drawAt(3, 11, gsTitle2);
-  		renderer.drawAt(4, 11, gsTitle3);
-  		renderer.drawAt(5, 11, gsTitle4);
-  		renderer.drawAt(6, 11, gsTitle5);
+		String title1 = " _____                      ____                           ";
+		String title2 = "|_   _|__ _ __ _ __ ___    / ___| __ _ _ __ ___   ___  ___ ";
+		String title3 = "  | |/ _ \\ '__| '_ ` _ \\  | |  _ / _` | '_ ` _ \\ / _ \\/ __|";
+		String title4 = "  | |  __/ |  | | | | | | | |_| | (_| | | | | | |  __/\\__ \\";
+		String title5 = "  |_|\\___|_|  |_| |_| |_|  \\____|\\__,_|_| |_| |_|\\___||___/";
+		GlyphString gsTitle1 = menuBuilder.withBaseString(title1).withFgColor(0, 0, 255).build();
+		GlyphString gsTitle2 = menuBuilder.withBaseString(title2).withFgColor(0, 64, 255).build();
+		GlyphString gsTitle3 = menuBuilder.withBaseString(title3).withFgColor(0, 128, 255).build();
+		GlyphString gsTitle4 = menuBuilder.withBaseString(title4).withFgColor(0, 192, 255).build();
+		GlyphString gsTitle5 = menuBuilder.withBaseString(title5).withFgColor(0, 255, 255).build();
+		renderer.drawAt(2, 11, gsTitle1);
+		renderer.drawAt(3, 11, gsTitle2);
+		renderer.drawAt(4, 11, gsTitle3);
+		renderer.drawAt(5, 11, gsTitle4);
+		renderer.drawAt(6, 11, gsTitle5);
 	}
 
 	private GameBorder defaultGameBorder(Renderer renderer2) {
-		return new GameBorder.Builder(renderer.getCanvasHeight(), renderer.getCanvasWidth())
-				.withFgColor(100,100,255)
-				.withBgColor(100, 0, 255)
-				.withNoSidebar()
-				.build();
+		return new GameBorder.Builder(renderer.getCanvasHeight(), renderer.getCanvasWidth()).withFgColor(100, 100, 255)
+				.withBgColor(100, 0, 255).withNoSidebar().build();
 	}
 
-	private static void initEventSystem() {
-		instance.ked = new KeyboardEventDriver.Builder()
-				.withInputStream(System.in)
-				.withListener((KeyEvent ke) -> instance.handleKeyEvent(ke))
-				.build();
-		
-		instance.eventBus.subscribe(PlaySoundEvent.class, 
-				(PlaySoundEvent pse) -> {instance.handlePlaySoundEvent(pse);});
-		
-		instance.eventBus.subscribe(MidiStartEvent.class, 
-				(MidiStartEvent mse) -> {instance.handleMidiStartEvent(mse);});
-		
-		instance.eventBus.subscribe(MidiStopEvent.class, 
-				(MidiStopEvent mse) -> {instance.handleMidiStopEvent(mse);});
+	private static void initEventSystem(GameConsole instance) {
+		instance.ked = new KeyboardEventDriver.Builder().withInputStream(System.in)
+				.withListener((KeyEvent ke) -> instance.handleKeyEvent(ke)).build();
+
+		instance.eventBus.subscribe(PlaySoundEvent.class, (PlaySoundEvent pse) -> {
+			instance.handlePlaySoundEvent(pse);
+		});
+
+		instance.eventBus.subscribe(MidiStartEvent.class, (MidiStartEvent mse) -> {
+			instance.handleMidiStartEvent(mse);
+		});
+
+		instance.eventBus.subscribe(MidiStopEvent.class, (MidiStopEvent mse) -> {
+			instance.handleMidiStopEvent(mse);
+		});
+
+		instance.eventBus.subscribe(KeyDownEvent.class, (KeyDownEvent kde) -> {
+			instance.handleKeyDownEvent(kde);
+		});
 	}
-	
+
+	private void handleKeyDownEvent(KeyDownEvent kde) {
+		String key = kde.getKey();
+		if (!gameIsRunning) {
+			switch (key) {
+			case Keys.UP_ARROW:
+				gameIndex--;
+				currentGame = games.get(gameNames.get(gameIndex));
+				renderGameSelector(gameIndex);				
+				break;
+			case Keys.DOWN_ARROW:
+				gameIndex++;
+				currentGame = games.get(gameNames.get(gameIndex));
+				renderGameSelector(gameIndex);
+				break;
+			case Keys.ENTER:
+				gameIsRunning = true;
+				currentGame.plugIn(this);
+				instance.renderMenu();
+				instance.renderGameSelector(instance.gameIndex);
+				gameIsRunning = false;
+				break;
+			case "Q":
+			case "q":
+				stillPlaying = false;
+				break;
+			}
+		}
+	}
+
 	private void handleMidiStopEvent(MidiStopEvent mse) {
 		instance.soundPlayer.stopMidi();
 	}
 
 	private void handleMidiStartEvent(MidiStartEvent mse) {
 		String path = mse.getPath();
-		byte[] cached = getFileBytes(path);					
+		byte[] cached = getFileBytes(path);
 		instance.soundPlayer.playMidi(new ByteArrayInputStream(cached), true);
 	}
 
-	private void handlePlaySoundEvent(PlaySoundEvent pse) {		
-			String path = pse.getPath();
-			byte[] cached = getFileBytes(path);					
-			instance.soundPlayer.play(new ByteArrayInputStream(cached));
+	private void handlePlaySoundEvent(PlaySoundEvent pse) {
+		String path = pse.getPath();
+		byte[] cached = getFileBytes(path);
+		instance.soundPlayer.play(new ByteArrayInputStream(cached));
 	}
 
-	private byte[] getFileBytes(String path){
-		try{
+	private byte[] getFileBytes(String path) {
+		try {
 			byte[] cached = cachedBytes.get(path);
-			if(cached == null){
+			if (cached == null) {
 				InputStream sound = classLoader.getResourceAsStream(path);
-				
-				ByteArrayOutputStream buffer = new ByteArrayOutputStream(); 
-				int nextByte = sound.read(); 
-				while(nextByte != -1){ 
-					buffer.write(nextByte); 
-					nextByte = sound.read(); 
-				} 
+
+				ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+				int nextByte = sound.read();
+				while (nextByte != -1) {
+					buffer.write(nextByte);
+					nextByte = sound.read();
+				}
 				cached = buffer.toByteArray();
 				cachedBytes.put(path, cached);
 			}
 			return cached;
-		} catch(Exception ex){
-			//gulp
+		} catch (Exception ex) {
+			// gulp
 		}
 		return null;
 	}
 
-	public EventBus getEventBus(){
+	public EventBus getEventBus() {
 		return this.eventBus;
 	}
-	
-	public void runInThreadPool(Runnable runnable){
+
+	public void runInThreadPool(Runnable runnable) {
 		pool.execute(runnable);
 	}
-	
-	
+
 	private void handleKeyEvent(KeyEvent ke) {
-		if(ke.getKeyEventType() == KeyEventType.UP){
+		if (ke.getKeyEventType() == KeyEventType.UP) {
 			eventBus.fire((KeyUpEvent) ke);
 		}
-		if(ke.getKeyEventType() == KeyEventType.DOWN){
+		if (ke.getKeyEventType() == KeyEventType.DOWN) {
 			eventBus.fire((KeyDownEvent) ke);
 		}
 	}
@@ -248,12 +305,12 @@ public class GameConsole {
 	public Renderer getRenderer() {
 		return renderer;
 	}
-	
+
 	private SoundPlayer getSoundPlayer() {
 		Sequencer sequencer = null;
-		try{
+		try {
 			sequencer = MidiSystem.getSequencer();
-		} catch(Exception ex){
+		} catch (Exception ex) {
 			// gulp
 		}
 
--- src/main/java/whorten/termgames/utils/Keys.java
+++ src/main/java/whorten/termgames/utils/Keys.java
@@ -6,6 +6,7 @@ public class Keys {
 	public static final String UP_ARROW = "\u0026";
 	public static final String RIGHT_ARROW = "\u0027";
 	public static final String DOWN_ARROW = "\u0028";
+	public static final String ENTER = "\n";
 	
 	public static final int LEFT_ARROW_BYTE = 0x25;
 	public static final int UP_ARROW_BYTE = 0x26;
