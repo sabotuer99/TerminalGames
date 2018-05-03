e5ce3e66e6143c75b355f71534da4728bdbe10ff
troy <sabotuer99@gmail.com>
2018-05-02 23:15:27 -0700

    started class GlyphLoader to load glyph representations from a stream

---
+++ src/main/java/whorten/termgames/games/tableflipper/renderer/TableFlipperRenderer.java
@@ -0,0 +1,5 @@
+package whorten.termgames.games.tableflipper.renderer;
+
+public class TableFlipperRenderer {
+
+}
---
+++ src/test/java/whorten/termgames/glyphs/GlyphLoaderTests.java
@@ -0,0 +1,5 @@
+package whorten.termgames.glyphs;
+
+public class GlyphLoaderTests {
+
+}
