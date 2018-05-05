ceb6baf8ae0fbcee8e33041cd0115f298d3454ba
troy <sabotuer99@gmail.com>
2018-05-04 22:20:12 -0700

    added tests for CoordRect

--- src/test/java/whorten/termgames/geometry/CoordRectTests.java
+++ src/test/java/whorten/termgames/geometry/CoordRectTests.java
@@ -1,5 +1,25 @@
 package whorten.termgames.geometry;
 
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.Test;
+
 public class CoordRectTests {
 
+	@Test
+	public void new3x3Rect(){
+		CoordRect sut = new CoordRect(new Coord(0,0),new Coord(2,2));
+		
+		assertThat(sut.size()).isEqualTo(8);
+		assertThat(sut).containsAllOf(
+				new Coord(0,0),
+				new Coord(1,0),
+				new Coord(2,0),
+				new Coord(0,1),
+				new Coord(2,1),
+				new Coord(0,2),
+				new Coord(1,2),
+				new Coord(2,2)
+				);
+	}
 }
