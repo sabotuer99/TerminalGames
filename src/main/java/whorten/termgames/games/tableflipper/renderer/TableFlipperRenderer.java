169f47cf7bc824267206ea03171ef399ea587a39
troy <sabotuer99@gmail.com>
2018-05-20 20:04:17 -0700

    added tableflipper background file, started renderer

--- src/main/java/whorten/termgames/games/tableflipper/renderer/TableFlipperRenderer.java
+++ src/main/java/whorten/termgames/games/tableflipper/renderer/TableFlipperRenderer.java
@@ -1,6 +1,22 @@
 package whorten.termgames.games.tableflipper.renderer;
 
+import java.util.Set;
+
+import whorten.termgames.GameConsole;
+import whorten.termgames.glyphs.collate.GlyphStringCoord;
+
 public class TableFlipperRenderer {
 
+	private static final String TABLEFLIPPER_BOARD_FILE = "";
+	private GameConsole console;
+
+	public TableFlipperRenderer(GameConsole console){
+		this.console = console;
+	}
+	
+	public void drawBoard(){
+		Set<GlyphStringCoord> g = console.loadFromFile(TABLEFLIPPER_BOARD_FILE);
+		console.getRenderer().drawGlyphStringCollection(g);
+	}
 	
 }
